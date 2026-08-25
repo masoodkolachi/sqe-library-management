bd54dfcccebbef1d39032684f85ccec630a9146a
Masood Ahmed <kolachimasood77@gmail.com>
2026-08-25 14:20:57 +0500

    Modify student grades and fix average calculation type
    Updated student grades and modified overall average calculation.

--- src/gradebook/studentGradebook.java
+++ src/gradebook/studentGradebook.java
@@ -24,12 +24,12 @@ public class studentGradebook {
                 new Student("Ali", new int[] { 80, 75, 90, 85, 88 }),
                 new Student("Ahmed", new int[] { 70, 65, 78, 72, 75 }),
                 new Student("Sara", new int[] { 92, 88, 95, 90, 94 }),
-                new Student("Ayesha", new int[] { 85, 80, 82, 88, 90 }),
+                new Student("Ayesha", new int[] { 0}),
                 new Student("Hassan", new int[] { 60, 68, 65, 70, 62 }),
-                new Student("Fatima", new int[] { 78, 82, 80, 75, 85 }),
-                new Student("Usman", new int[] { 88, 85, 90, 87, 92 }),
+                new Student("Fatima", new int[] { 78, -56, 80, 75, 85 }),
+                new Student("usman", new int[] { 88, 85, 90, 87, 92 }),
                 new Student("Zainab", new int[] { 95, 92, 96, 94, 90 }),
-                new Student("Bilal", new int[] { 72, 70, 68, 75, 74 }),
+                new Student("Bilal", new int[] { 72, 70, 68, -75, 74 }),
                 new Student("Mariam", new int[] { 84, 86, 80, 82, 88 })
         };
 
@@ -44,7 +44,7 @@ public class studentGradebook {
             totalAverage += average;
         }
 
-        double overallAverage = totalAverage / students.length;
+        int overallAverage = totalAverage / students.length;
 
         System.out.println("\nOverall Average: " + overallAverage);
     }
